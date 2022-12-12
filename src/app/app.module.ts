cc5e8bd9ecac75ec1b40cdc37742c375caeea5e8
Michelle Perez Morales <michelle.perez@sngular.com>
2022-12-12 17:20:47 -0500

    fisrt

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,13 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { TabComponent } from './components/tab/tab.component';
+import { TabsContainerComponent } from './components/tabs-container/tabs-container.component';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
+  declarations: [AppComponent, TabComponent, TabsContainerComponent],
+  imports: [BrowserModule, AppRoutingModule],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
